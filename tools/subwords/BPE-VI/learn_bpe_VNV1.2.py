13b56a6bbf930f5eafc959f72e5b433f82601833
ngovinhtn <ntvinh@ictu.edu.vn>
2020-02-16 22:35:30 +0700

    update BPE-VN

--- tools/subwords/BPE-VI/learn_bpe_VNV1.2.py
+++ tools/subwords/BPE-VI/learn_bpe_VNV1.2.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# Author: Ngo Vinh 
+# Author: Ngo Vinh
 
 """
 Learn BPE with 2-gram from data training
 
-Run: python learn_bpe_VNV1.1.py -i train.ja-vi.vi -o train.ja-vi.bpe.vi -c codes.vi -s 1 -l log.txt 
+Run: python learn_bpe_VNV1.1.py -i train.ja-vi.vi -o train.ja-vi.bpe.vi -c codes.vi -s 1 -l log.txt
 """
 
 from __future__ import unicode_literals
@@ -25,6 +25,7 @@ import time
 import operator
 argparse.open = open
 
+import numpy
 
 def create_parser():
     parser = argparse.ArgumentParser(
@@ -48,10 +49,10 @@ def create_parser():
         metavar='PATH',
         help="log file ")
     parser.add_argument(
-        '--symbols', '-s', type=int, default=10000,
+        '--num_get_pairs', '-s', type=int, default=2,
         help="Create this many new symbols (each representing a character n-gram) (default: %(default)s))")
     parser.add_argument(
-        '--min-frequency', type=int, default=2, metavar='FREQ',
+        '--min-frequency', '-m', type=int, default=100, metavar='FREQ',
         help='Stop if no symbol pair has frequency >= FREQ (default: %(default)s))')
     parser.add_argument('--dict-input', action="store_true",
                         help="If set, input file is interpreted as a dictionary where each line contains a word-count pair")
@@ -107,17 +108,23 @@ class Thread_Write_Vocab_Code (threading.Thread):
         fvocab.write(self.vocab+'\n')
         fvocab.close()
 
-def get_pairs_most_freq(content):
+def get_pairs_most_freq(content,min_frequency):
     pairs = collections.defaultdict(int)
     for line in content.splitlines():
         prevWord = None
-        for word in line.split():
-            if prevWord not in [None,'♫','“','”','&amp;',',','.','-','?',')','(','quot',';','--',':','&quot;','quot','và','nhưng','/','tôi','bạn'] and prevWord.isdigit()== False:
-                if word not in [None,'♫','“','”','&amp;',',','.','-','?',')','(','quot',';','--',':','&quot;','quot','và','nhưng','/','tôi','bạn'] and prevWord.isdigit()== False:
+        for word in line.split():   #'và','nhưng','/','tôi','bạn'
+            if prevWord not in [None,'♫','“','”','&amp;',',','.','-','?',')','(','quot',';','--',':','&quot;','quot','và','nhưng','/','tôi','bạn','Tôi','Bạn'] and prevWord.isdigit()== False:
+                if word not in [None,'♫','“','”','&amp;',',','.','-','?',')','(','quot',';','--',':','&quot;','quot','và','nhưng','/','tôi','bạn','Tôi','Bạn'] and word.isdigit()== False:
                     pairs[prevWord, word] += 1
+            # if prevWord=="Tây" and word =="Ban":
+            #     print(prevWord,word)
+
             prevWord = word
     # f.close()
-    pairs=dict((k,v) for k,v in pairs.items() if v > 2)
+    # print(pairs["Tây", "Ban"]) #80
+    pairs=dict((k,v) for k,v in pairs.items() if v >= min_frequency)
+    # for k,v in pairs.items():
+    #     print(k," ",v)
     # print('_____________________________________________________')
     # get most freq
     # best_pair_freq = max(pairs,key=pairs.get)
@@ -131,17 +138,18 @@ def update_pairs(best,content,fvocab):
     # ket hop vao pair roi cuoi cung in phan ket hop cua pair
     wordcat = " "+best[0] + "_" + best[1]+" "
     wordsub = " "+best[0] + " " + best[1]+" "
+    # if best[0]=="Tây" and best[1]=="Ban":
+    #     print(wordsub)
     replaced=content
     replaced = content.replace(wordsub, wordcat)
-    thread1 = Thread_Write_Vocab_Code(fvocab,wordcat)
+    thread1 = Thread_Write_Vocab_Code(fvocab,wordsub.strip())
     thread1.start()
     # print(replaced)
 
     return replaced
 
 
-
-def main(infile, outfile, codefile, logfile, num_symbols,  min_frequency=2, verbose=False, is_dict=False):
+def main(infile, outfile, codefile, logfile, num_get_pairs,  min_frequency, verbose=False, is_dict=False):
     """Learn num_symbols BPE operations from vocabulary, and write to outfile.
     """
     f1 = open(infile.name, 'r')
@@ -161,22 +169,45 @@ def main(infile, outfile, codefile, logfile, num_symbols,  min_frequency=2, verb
     opera=0
     thread1 = Thread_Print(0,0,('',''),0,logfile)
     thread1.start()
-    # number_symbols is the number get pairs
-    for i in range(num_symbols):
-        pairs = get_pairs_most_freq(content)
-        # pairs1=dict(sorted(pairs.items(), key=operator.itemgetter(1),reverse=True))
-        pairs1=dict((v,k) for k,v in pairs.items())
-        for v,k in sorted(pairs1.items(),reverse=True):
+    # num_get_pairs is the number get pairs
+
+
+    # for i in range(num_get_pairs):
+
+    while opera < num_get_pairs:
+        # if i==0:
+        pairs = get_pairs_most_freq(content,min_frequency)
+        # else:
+        #     min_frequency=min_frequency*2
+        #     pairs = get_pairs_most_freq(content,min_frequency)
+        # print("pairs: ", pairs)
+        # for v,k in pairs.items():
+        #     print(k," ",v)
+
+        words = list(pairs.keys())
+        freqs = list(pairs.values())
+        sorted_idx = numpy.argsort(freqs)
+        # print(sorted_idx)
+        # pairs1=dict((v,k) for k,v in pairs.items())
+        # print(pairs1)
+        for ii in sorted_idx[::-1]:
+                                    # for v,k in sorted(pairs1.items(),reverse=True):
             # print(k,v)
-            content1=""
-            content1=update_pairs(k,content,codefile)
-            content=content1
-            opera = opera +1
-            thread = Thread_Print(0,2,k,v,logfile)
-            thread.start()
-            if opera % 2000 == 0:
-                thread2 = Thread_Print(opera,1,('',''),0,logfile)
-                thread2.start()
+            # if k[0]=='Tây' and k[1]=='Ban':
+            #     print(k)
+            if opera < num_get_pairs:
+                content1=""
+
+                content1=update_pairs(words[ii],content,codefile)
+                content=content1
+                opera = opera +1
+                thread = Thread_Print(0,2,words[ii],freqs[ii],logfile)
+                thread.start()
+                if opera % 2000 == 0:
+                    thread2 = Thread_Print(opera,1,('',''),0,logfile)
+                    thread2.start()
+            else:
+                break
 
 
     # remove space at the beginning and the end of file, and write to file
@@ -205,9 +236,9 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     # read/write files as UTF-8
-    # if args.input.name != '<stdin>':
-    #     args.input = codecs.open(args.input.name, encoding='utf-8')
-    # if args.output.name != '<stdout>':
-    #     args.output = codecs.open(args.output.name, 'w', encoding='utf-8')
+    if args.input.name != '<stdin>':
+        args.input = codecs.open(args.input.name, encoding='utf-8')
+    if args.output.name != '<stdout>':
+        args.output = codecs.open(args.output.name, 'w', encoding='utf-8')
 
-    main(args.input, args.output, args.codefile,args.logfile, args.symbols, args.min_frequency, args.verbose, is_dict=args.dict_input)
+    main(args.input, args.output, args.codefile,args.logfile, args.num_get_pairs, args.min_frequency, args.verbose, is_dict=args.dict_input)
